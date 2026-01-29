197624bcee2419a2dbca0f9973e53a72f7f7161e
mariapomileva-alt <maria.pomileva@gmail.com>
2026-01-29 13:39:02 +0200

    Update config.js
    s

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export const SUPABASE_URL = "https://ygkihopdluebecdvaoqe.supabase.co";
 export const SUPABASE_ANON_KEY = "sb_publishable_u0fslqR17Gq6HqK-oi4vRg_ders4QXv";
-export const ADMIN_PASSWORD = "ВАШ_ПАРОЛЬ";
+export const ADMIN_PASSWORD = "CHANGE_ME";
